611b77b8962f378ba6de16f50c45acc0b869291b
ankithvarun <ankith.varun@research.iiit.ac.in>
2021-10-20 21:06:25 +0530

    Fixed order

--- dump.sql
+++ dump.sql
@@ -2,246 +2,297 @@ DROP DATABASE IF EXISTS `FPL`;
 CREATE SCHEMA `FPL`;
 USE `FPL`;
 
-DROP TABLE IF EXISTS `TYPE`;
-CREATE TABLE `TYPE` (
-  `League_code` <type>,
-  `League_Type` <type>,
-  PRIMARY KEY (`League_Type`)
-);
-
-DROP TABLE IF EXISTS `COMPETES`;
-CREATE TABLE `COMPETES` (
-  `Team_name` <type>,
-  `League_code` <type>,
-  `Rank` <type>
-);
-
-DROP TABLE IF EXISTS `PLAYS`;
-CREATE TABLE `PLAYS` (
-  `Team_name` <type>,
-  `Gameweek_number` <type>,
-  `Player_name` <type>,
-  `Is_captain` <type>,
-  `Is_vice_captain` <type>,
-  `Is_starting` <type>
-);
-
-DROP TABLE IF EXISTS `FORWARD`;
-CREATE TABLE `FORWARD` (
-  `Player_name` <type>,
-  `Shots_on_target` <type>,
-  `Touches_inside_box` <type>,
-  `Dribbles` <type>,
-  PRIMARY KEY (`Player_name`)
-);
-
-DROP TABLE IF EXISTS `FIXTURE2b`;
-CREATE TABLE `FIXTURE2b` (
-  `Stadium` <type>,
-  `City` <type>,
-  PRIMARY KEY (`Stadium`)
+DROP TABLE IF EXISTS `TEAM`;
+CREATE TABLE `TEAM` (
+  `Name` varchar(100) NOT NULL,
+  `Manager_First_name` varchar(100) NOT NULL,
+  `Manager_Last_name` varchar(100) NOT NULL,
+  `Nationality` varchar(40) NOT NULL,
+  `Total Points` int DEFAULT 0,
+  `Squad Value` decimal(5,2) NOT NULL,
+  `Transfers_left` int DEFAULT 1,
+  `Money_left` decimal(5,2) NOT NULL,
+  PRIMARY KEY (`Name`),
+  UNIQUE KEY `Manager_name`(`Manager_First_name`, `Manager_Last_name`),
+  CHECK (`Transfers_left` <= 2),
+  CHECK (`Squad Value` > 0)
 );
 
 DROP TABLE IF EXISTS `GAMEWEEK`;
 CREATE TABLE `GAMEWEEK` (
-  `Week_number` <type>,
-  `Highest_points` <type>,
-  `Average_points` <type>,
-  `Month` <type>,
-  `Day` <type>,
-  `Hours` <type>,
-  `Minutes` <type>,
-  PRIMARY KEY (`Week_number`)
-);
-
-DROP TABLE IF EXISTS `TEAM`;
-CREATE TABLE `TEAM` (
-  `Name` <type>,
-  `Manager_First_name` <type>,
-  `Manager_Last_name` <type>,
-  `Nationality` <type>,
-  `Total Points` <type>,
-  `Squad Value` <type>,
-  `Transfers_left` <type>,
-  `Money_left` <type>,
-  PRIMARY KEY (`Name`),
-  KEY `CK` (`Manager_First_name`, `Manager_Last_name`)
-);
-
-DROP TABLE IF EXISTS `MIDFIELDER`;
-CREATE TABLE `MIDFIELDER` (
-  `Player_name` <type>,
-  `Passing Accuracy` <type>,
-  `Chances Created` <type>,
-  `Through_balls` <type>,
-  PRIMARY KEY (`Player_name`)
-);
-
-DROP TABLE IF EXISTS `PLAYS_IN`;
-CREATE TABLE `PLAYS_IN` (
-  `Player_name` <type>,
-  `Home_club` <type>,
-  `Away_club` <type>,
-  `Week_number` <type>,
-  `Player_points` <type>
-);
-
-DROP TABLE IF EXISTS `SPONSORS`;
-CREATE TABLE `SPONSORS` (
-  `Partner_name` <type>,
-  `Season` <type>
-);
-
-DROP TABLE IF EXISTS `CATEGORY`;
-CREATE TABLE `CATEGORY` (
-  `Partner_name` <type>,
-  `Partner_category` <type>,
-  PRIMARY KEY (`Partner_category`)
-);
-
-DROP TABLE IF EXISTS `ADD_POINTS1`;
-CREATE TABLE `ADD_POINTS1` (
-  `Team_name` <type>,
-  `Week_number` <type>,
-  `Player_name` <type>,
-  `Home_club` <type>,
-  `Away_club` <type>
-);
-
-DROP TABLE IF EXISTS `LEAGUE`;
-CREATE TABLE `LEAGUE` (
-  `League_code` <type>,
-  `Name` <type>,
-  `Number_of_teams` <type>,
-  PRIMARY KEY (`League_code`),
-  KEY `CK` (`Name`)
-);
-
-DROP TABLE IF EXISTS `DEFENDER`;
-CREATE TABLE `DEFENDER` (
-  `Player_name` <type>,
-  `Clearances` <type>,
-  `Tackles` <type>,
-  `Interceptions` <type>,
-  PRIMARY KEY (`Player_name`)
-);
-
-DROP TABLE IF EXISTS `TRANSFER`;
-CREATE TABLE `TRANSFER` (
-  `Year` <type>,
-  `Month` <type>,
-  `Day` <type>,
-  `Hours` <type>,
-  `Minutes` <type>,
-  `Player_in` <type>,
-  `Player_out` <type>,
-  `Team_name` <type>,
-  `Week_number` <type>,
-  PRIMARY KEY (`Player_in`, `Player_out`)
-);
-
-DROP TABLE IF EXISTS `HEAD_TO_HEAD`;
-CREATE TABLE `HEAD_TO_HEAD` (
-  `Teamname_1` <type>,
-  `Teamname_2` <type>,
-  `League_code` <type>,
-  `     Result` <type>
-);
-
-DROP TABLE IF EXISTS `ACTIVATES`;
-CREATE TABLE `ACTIVATES` (
-  `Team_name` <type>,
-  `Week_number` <type>,
-  `Chip_name` <type>,
-  `Gameweek Points` <type>
-);
-
-DROP TABLE IF EXISTS `EDITION`;
-CREATE TABLE `EDITION` (
-  `Season` <type>,
-  `Winner` <type>,
-  `Runners_up` <type>,
-  PRIMARY KEY (`Season`)
-);
-
-DROP TABLE IF EXISTS `CHIP`;
-CREATE TABLE `CHIP` (
-  `Name` <type>,
-  PRIMARY KEY (`Name`)
-);
-
-DROP TABLE IF EXISTS `ADD_POINTS2`;
-CREATE TABLE `ADD_POINTS2` (
-  `Player_name` <type>,
-  `Home_club` <type>,
-  `Away_club` <type>,
-  `Player_points` <type>
-);
-
-DROP TABLE IF EXISTS `KEEPER`;
-CREATE TABLE `KEEPER` (
-  `Player_name` <type>,
-  `Saves` <type>,
-  `Clean_sheets` <type>,
-  `Penalties Saved` <type>,
-  PRIMARY KEY (`Player_name`)
-);
-
-DROP TABLE IF EXISTS `PARTNER1`;
-CREATE TABLE `PARTNER1` (
-  `Name` <type>,
-  `City` <type>,
-  PRIMARY KEY (`Name`)
-);
-
-DROP TABLE IF EXISTS `HAS_ACTIVE`;
-CREATE TABLE `HAS_ACTIVE` (
-  `Team_name` <type>,
-  `Chip_name` <type>
-);
-
-DROP TABLE IF EXISTS `FIXTURE2a`;
-CREATE TABLE `FIXTURE2a` (
-  `Home_club` <type>,
-  `Stadium` <type>
-);
-
-DROP TABLE IF EXISTS `KINGS_OF_THE_GAMEWEEK`;
-CREATE TABLE `KINGS_OF _THE_GAMEWEEK` (
-  `Player_name` <type>,
-  `Week_number` <type>
-);
-
-DROP TABLE IF EXISTS `PLAYER`;
-CREATE TABLE `PLAYER` (
-  `Name` <type>,
-  `Club` <type>,
-  `Market_cost` <type>,
-  `Form` <type>,
-  `Total_points` <type>,
-  `Selection %` <type>,
-  `Fitness Status` <type>,
-  PRIMARY KEY (`Name`)
+  `Week_number` int(2) DEFAULT 1,
+  `Highest_points` int NOT NULL,
+  `Average_points` int NOT NULL,
+  `Month(2)` int NOT NULL,
+  `Day(2)` int NOT NULL,
+  `Hours(2)` int NOT NULL,
+  `Minutes(2)` int NOT NULL,
+  PRIMARY KEY (`Week_number`),
+  CHECK (`Week_number` <= 38)
 );
 
 DROP TABLE IF EXISTS `FIXTURE1`;
 CREATE TABLE `FIXTURE1` (
-  `Home_club` <type>,
-  `Away_club` <type>,
-  `Result` <type>,
-  `Year` <type>,
-  `Month` <type>,
-  `Day` <type>,
-  `Hours` <type>,
-  `Minutes` <type>,
-  `Week_number` <type>,
-  PRIMARY KEY (`Home_club`, `Away_club`)
+  `Home_club` varchar(50) NOT NULL,
+  `Away_club` varchar(50) NOT NULL,
+  `Result` varchar(100) NOT NULL,
+  `Year` int(2) NOT NULL,
+  `Month` int(2) NOT NULL,
+  `Day` int(2) NOT NULL,
+  `Hours` int(2) NOT NULL,
+  `Minutes` int(2) NOT NULL,
+  `Week_number` int(2) NOT NULL,
+  PRIMARY KEY (`Home_club`, `Away_club`),
+  FOREIGN KEY (`Week_number`) REFERENCES `GAMEWEEK`(`Week_number`),
+  CHECK (`Week_number` <= 38)
 );
 
+DROP TABLE IF EXISTS `FIXTURE2b`;
+CREATE TABLE `FIXTURE2b` (
+  `Stadium` varchar(50) NOT NULL,
+  `City` varchar(50) NOT NULL,
+  PRIMARY KEY (`Stadium`)
+);
+
+DROP TABLE IF EXISTS `FIXTURE2a`;
+CREATE TABLE `FIXTURE2a` (
+  `Home_club` varchar(50) NOT NULL,
+  `Stadium` varchar(50) NOT NULL,
+  PRIMARY KEY(`Home_club`,`Stadium`),
+  FOREIGN KEY (`Home_club`) REFERENCES `FIXTURE1`(`Home_club`),
+  FOREIGN KEY (`Stadium`) REFERENCES `FIXTURE2b`(`Stadium`) 
+);
+
+DROP TABLE IF EXISTS `PLAYER`;
+CREATE TABLE `PLAYER` (
+  `Name` varchar(100) NOT NULL,
+  `Club` varchar(100) NOT NULL,
+  `Market_cost` decimal(5,2) NOT NULL,
+  `Form` int NOT NULL,
+  `Total_points` int NOT NULL,
+  `Selection %` decimal(4,2),
+  `Fitness Status` varchar(100) NOT NULL,
+  PRIMARY KEY (`Name`)
+);
+
+DROP TABLE IF EXISTS `FORWARD`;
+CREATE TABLE `FORWARD` (
+  `Player_name` varchar(100) NOT NULL,
+  `Shots_on_target` int,
+  `Touches_inside_box` int,
+  `Dribbles` int,
+  PRIMARY KEY (`Player_name`)
+);
+
+DROP TABLE IF EXISTS `MIDFIELDER`;
+CREATE TABLE `MIDFIELDER` (
+  `Player_name` varchar(100) NOT NULL,
+  `Passing Accuracy` int,
+  `Chances Created` int,
+  `Through_balls` int,
+  PRIMARY KEY (`Player_name`)
+);
+
+DROP TABLE IF EXISTS `DEFENDER`;
+CREATE TABLE `DEFENDER` (
+  `Player_name` varchar(100) NOT NULL,
+  `Clearances` int,
+  `Tackles` int,
+  `Interceptions` int,
+  PRIMARY KEY (`Player_name`)
+);
+
+DROP TABLE IF EXISTS `KEEPER`;
+CREATE TABLE `KEEPER` (
+  `Player_name` varchar(100) NOT NULL,
+  `Saves` int,
+  `Clean_sheets` int,
+  `Penalties Saved` int,
+  PRIMARY KEY (`Player_name`)
+);
+
+DROP TABLE IF EXISTS `LEAGUE`;
+CREATE TABLE `LEAGUE` (
+  `League_code` char(6) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `Number_of_teams` int(6) DEFAULT 0,
+  PRIMARY KEY (`League_code`),
+  UNIQUE KEY `League_name` (`Name`)
+);
+
+DROP TABLE IF EXISTS `TYPE`;
+CREATE TABLE `TYPE` (
+  `League_code` char(6) NOT NULL,
+  `League_Type` varchar(20) NOT NULL,
+  PRIMARY KEY (`League_code`, `League_Type`),
+  CHECK (`League_Type`='Classic' OR `League_Type`='Public' OR `League_Type`='Invitational' OR `League_Type`='Head-to-Head' OR `League_Type`='General'), 
+  FOREIGN KEY (`League_code`) REFERENCES `LEAGUE` (`League_code`)
+);
+
+DROP TABLE IF EXISTS `CHIP`;
+CREATE TABLE `CHIP` (
+  `Name` varchar(15) NOT NULL,
+  PRIMARY KEY (`Name`)
+);
+
+DROP TABLE IF EXISTS `TRANSFER`;
+CREATE TABLE `TRANSFER` (
+  `Year` int(4) NOT NULL,
+  `Month` int(2) NOT NULL,
+  `Day` int(2) NOT NULL,
+  `Hours` int(2) NOT NULL,
+  `Minutes` int(2) NOT NULL,
+  `Player_in` varchar(100) NOT NULL,
+  `Player_out` varchar(100) NOT NULL,
+  `Team_name` varchar(100) NOT NULL,
+  `Week_number` int(2) NOT NULL,
+  PRIMARY KEY (`Player_in`, `Player_out`, `Team_name`, `Week_number`),
+  FOREIGN KEY (`Team_name`) REFERENCES `TEAM` (`Name`),
+  FOREIGN KEY (`Week_number`) REFERENCES `GAMEWEEK` (`Week_number`)
+);
+ 
+DROP TABLE IF EXISTS `COMPETES`;
+CREATE TABLE `COMPETES` (
+  `Team_name` varchar(100) NOT NULL,
+  `League_code` char(6) NOT NULL,
+  `Rank` int,
+  PRIMARY KEY(`Team_name`,`League_code`),
+  FOREIGN KEY (`Team_name`) REFERENCES `TEAM` (`Name`),
+  FOREIGN KEY (`League_code`) REFERENCES `LEAGUE` (`League_code`)
+);
+ 
+DROP TABLE IF EXISTS `PLAYS`;
+CREATE TABLE `PLAYS` (
+  `Team_name` varchar(100) NOT NULL,
+  `Gameweek_number` int(2) DEFAULT 1,
+  `Player_name` varchar(100) NOT NULL,
+  `Is_captain` boolean ,
+  `Is_vice_captain` boolean,
+  `Is_starting` boolean,
+  PRIMARY KEY(`Team_name`, `Gameweek_number`, `Player_name`),
+  FOREIGN KEY (`Team_name`) REFERENCES `TEAM`(`Name`),
+  FOREIGN KEY (`Gameweek_number`) REFERENCES `GAMEWEEK`(`Week_number`),
+  FOREIGN KEY (`Player_name`) REFERENCES `PLAYER`(`Name`),
+  CHECK (`Gameweek_number` <= 38)
+);
+ 
+DROP TABLE IF EXISTS `PLAYS_IN`;
+CREATE TABLE `PLAYS_IN` (
+  `Player_name` varchar(100) NOT NULL,
+  `Home_club` varchar(100) NOT NULL,
+  `Away_club` varchar(100) NOT NULL,
+  `Week_number` int NOT NULL,
+  `Player_points` int,
+  FOREIGN KEY (`Player_name`) REFERENCES `PLAYER`(`Name`),
+  FOREIGN KEY (`Home_club`, `Away_club`) REFERENCES `FIXTURE1`(`Home_club`, `Away_club`),
+  FOREIGN KEY (`Week_number`) REFERENCES `GAMEWEEK`(`Week_number`),
+  CHECK (`Week_number` <= 38) 
+);
+ 
+DROP TABLE IF EXISTS `ADD_POINTS1`;
+CREATE TABLE `ADD_POINTS1` (
+  `Team_name` varchar(100) NOT NULL,
+  `Week_number` int(2) NOT NULL,
+  `Player_name` varchar(100) NOT NULL,
+  `Home_club` varchar(100) NOT NULL,
+  `Away_club` varchar(100) NOT NULL,
+  FOREIGN KEY (`Team_name`) REFERENCES `TEAM`(`Name`),
+  FOREIGN KEY (`Week_number`) REFERENCES `GAMEWEEK`(`Week_number`),
+  FOREIGN KEY (`Player_name`) REFERENCES `PLAYER`(`Name`),
+  FOREIGN KEY (`Home_club`, `Away_club`) REFERENCES `FIXTURE1`(`Home_club`, `Away_club`),
+  CHECK (`Week_number` <= 38) 
+);
+ 
+DROP TABLE IF EXISTS `HEAD_TO_HEAD`;
+CREATE TABLE `HEAD_TO_HEAD` (
+  `Teamname_1` varchar(100) NOT NULL,
+  `Teamname_2` varchar(100) NOT NULL,
+  `League_code` char(6) NOT NULL,
+  `Result` varchar(10),
+  PRIMARY KEY (`Teamname_1`, `Teamname_2`, `League_code`),
+  FOREIGN KEY (`Teamname_1`) REFERENCES `TEAM` (`Name`),
+  FOREIGN KEY (`Teamname_2`) REFERENCES `TEAM` (`Name`),
+  FOREIGN KEY (`League_code`) REFERENCES `LEAGUE` (`League_code`)
+);
+ 
+DROP TABLE IF EXISTS `ACTIVATES`;
+CREATE TABLE `ACTIVATES` (
+  `Team_name` varchar(100) NOT NULL,
+  `Week_number` int(2) NOT NULL,
+  `Chip_name` varchar(15) NOT NULL,
+  `Gameweek Points` int,
+  PRIMARY KEY (`Team_name`, `Week_number`, `Chip_name`),
+  FOREIGN KEY (`Team_name`) REFERENCES `TEAM` (`Name`),
+  FOREIGN KEY (`Week_number`) REFERENCES `GAMEWEEK` (`Week_number`),
+  FOREIGN KEY (`Chip_name`) REFERENCES `CHIP` (`Name`)
+);
+ 
+DROP TABLE IF EXISTS `ADD_POINTS2`;
+CREATE TABLE `ADD_POINTS2` (
+  `Player_name` varchar(100) NOT NULL,
+  `Home_club` varchar(100) NOT NULL,
+  `Away_club` varchar(100) NOT NULL,
+  `Player_points` int,
+  PRIMARY KEY (`Player_name`, `Home_club`, `Away_club`),
+  FOREIGN KEY (`Player_name`) REFERENCES `PLAYER`(`Name`),
+  FOREIGN KEY (`Home_club`, `Away_club`) REFERENCES `FIXTURE1`(`Home_club`, `Away_club`)
+);	
+ 
+DROP TABLE IF EXISTS `HAS_ACTIVE`;
+CREATE TABLE `HAS_ACTIVE` (
+  `Team_name` varchar(100) NOT NULL,
+  `Chip_name` varchar(15) NOT NULL,
+  PRIMARY KEY (`Team_name`, `Chip_name`),
+  FOREIGN KEY (`Team_name`) REFERENCES `TEAM` (`Name`),
+  FOREIGN KEY (`Chip_name`) REFERENCES `CHIP` (`Name`)
+);
+ 
+DROP TABLE IF EXISTS `KINGS_OF_THE_GAMEWEEK`;
+CREATE TABLE `KINGS_OF _THE_GAMEWEEK` (
+  `Player_name` varchar(100) NOT NULL,
+  `Week_number` int(2) NOT NULL,
+  FOREIGN KEY (`Player_name`) REFERENCES `PLAYER`(`Name`),
+  FOREIGN KEY (`Week_number`) REFERENCES `GAMEWEEK`(`Week_number`),
+  CHECK (`Week_number` <= 38)
+);
+ 
 DROP TABLE IF EXISTS `PARTNER2`;
 CREATE TABLE `PARTNER2` (
-  `City` <type>,
-  `Country` <type>,
+  `City` varchar(50) NOT NULL,
+  `Country` varchar(50) NOT NULL,
   PRIMARY KEY (`City`)
+);
+ 
+DROP TABLE IF EXISTS `PARTNER1`;
+CREATE TABLE `PARTNER1` (
+  `Name` varchar(100) NOT NULL,
+  `City` varchar(50) NOT NULL,
+  PRIMARY KEY (`Name`),
+  FOREIGN KEY (`City`) REFERENCES `PARTNER2` (`City`)
+);
+
+DROP TABLE IF EXISTS `CATEGORY`;
+CREATE TABLE `CATEGORY` (
+  `Partner_name` varchar(100) NOT NULL,
+  `Partner_category` varchar(50) NOT NULL,
+  PRIMARY KEY (`Partner_category`,`Partner_name`),
+  FOREIGN KEY (`Partner_name`) REFERENCES `PARTNER1` (`Name`)
+);
+
+DROP TABLE IF EXISTS `EDITION`;
+CREATE TABLE `EDITION` (
+  `Season` int(4) NOT NULL,
+  `Winner` varchar(100),
+  `Runners_up` varchar(100),
+  PRIMARY KEY (`Season`)
+);
+
+DROP TABLE IF EXISTS `SPONSORS`;
+CREATE TABLE `SPONSORS` (
+  `Partner_name` varchar(100) NOT NULL,
+  `Season` int(4) NOT NULL,
+  PRIMARY KEY (`Partner_name`,`Season`),
+  FOREIGN KEY (`Partner_name`) REFERENCES `PARTNER1` (`Name`),
+  FOREIGN KEY (`Season`) REFERENCES `EDITION` (`Season`)
 );
 
