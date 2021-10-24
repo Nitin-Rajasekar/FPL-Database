0831730986e37966ead3d9d441b943af685728a5
ankithvarun <ankith.varun@research.iiit.ac.in>
2021-10-24 12:29:31 +0530

    Added main prompt code and schedule GW functionality

---
+++ FPL.py
@@ -0,0 +1,41 @@
+import pymysql
+import pymysql.cursors as cursors
+import subprocess as sp
+import insert as i
+
+# Main driver function
+def dispatch(ch):
+    if ch == 1:
+        i.scheduleGW(db,cur)
+    #elif ch == 2:
+
+    #elif ch == 3:
+
+
+# Establish database connection
+print('Welcome to Fantasy Premier League!!! Enter user credentials to login')
+Username=input('Username: ')
+Password=input('Password: ')
+db = pymysql.connect(host='localhost', port=3306, user='FPLadmin', password='FPL@admin@123', db='FPL', cursorclass=cursors.DictCursor)
+if(db.open):
+    print('Connected to FPL database')
+    input('Press any key to continue...')
+else:
+    print('Could not establish connection')
+    exit()
+
+# Prompt
+with db.cursor() as cur:
+    while(1):
+        tmp = sp.call('clear', shell=True)
+        print('1. Schedule upcoming gameweek')
+        print('2. Manage a team')
+        print('3. Update details')
+        print('4. Logout')
+        ch=int(input('Enter your choice: '))
+        tmp = sp.call('clear', shell=True)
+
+        if ch == 4:
+            exit()
+        else:
+            dispatch(ch)
---
+++ insert.py
@@ -0,0 +1,41 @@
+import subprocess as sp
+
+def scheduleGW(db,cur):
+    # Get the upcoming gameweek number
+    query = 'SELECT MAX(Week_number) AS GW FROM GAMEWEEK;'
+    cur.execute(query)
+    GW = int(cur.fetchone()['GW']) + 1
+    print('Scheduling Gameweek',str(GW)+'...')
+
+    # Insert into GAMEWEEK table
+    print('Enter the Gameweek Deadline')
+    day, month = map(str, input('Date (in DD-MM format): ').split('-'))
+    hrs, mins = map(str, input('Time (in hh:mm format): ').split(':'))
+    query = 'INSERT INTO GAMEWEEK(Week_number,Month,Day,Hours,Minutes) VALUES (%s,%s,%s,%s,%s)' % (GW,month,day,hrs,mins)
+    cur.execute(query)
+    db.commit()
+
+    # Get team squads from previous gameweek
+    query='INSERT IGNORE INTO PLAYS SELECT Team_name,%s,Player_name,Is_captain,Is_vice_captain,Is_starting FROM PLAYS AS P WHERE P.Gameweek_number=%s;' % (GW,GW-1)
+    cur.execute(query)
+    db.commit()
+
+    sp.call('clear', shell=True)
+    # Insert into FIXTURE table
+    print('Scheduling fixtures...')
+    n = int(input('Enter the number of fixtures to be scheduled: '))
+    for i in range(n):
+        sp.call('clear', shell=True)
+        print('Fixture',i+1)
+        HClub = input('Enter Home Club: ')
+        AClub = input('Enter Away Club: ')
+        print('Enter the Fixture date and time')
+        day, month, year = map(str, input('Date (in DD-MM-YYYY format): ').split('-'))
+        hrs, mins = map(str, input('Time (in hh:mm format): ').split(':'))
+        query = "INSERT INTO FIXTURE1(Home_club,Away_club,Year,Month,Day,Hours,Minutes,Week_number) VALUES ('%s','%s',%s,%s,%s,%s,%s,%s)" % (HClub,AClub,year,month,day,hrs,mins,GW)
+        cur.execute(query)
+        db.commit()
+
+    print('')
+    print('Finished scheduling Gameweek',GW)
+    input('Press any key to continue...')
